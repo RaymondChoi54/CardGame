4133f7837c64a65bfdce4b5576ff0c15e61f2e7c
RaymondChoi54 <concept_ray@hotmail.com>
2018-10-11 22:09:02 -0400

    Removed Comments

--- src/Main.java
+++ src/Main.java
@@ -5,55 +5,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		Card card = new Card(1, 1);
-//		Card card2 = new Card(2, 2);
-//		
-//		
-//		System.out.println(card);
-//		
-//		System.out.println(card.compareTo(card2));
-//		
-//		CardDeck deck = new CardDeck();
-//		
-//		for(int i = 0; i < 52; i++) {
-//			System.out.println(deck.nextCard());
-//		}
-//		
-//		deck.Shuffle();
-//		
-//		for(int i = 0; i < 52; i++) {
-//			try {
-//				System.out.println(deck.nextCard());
-//			} catch (Exception e) {
-//				System.out.println("No cards left");
-//			}
-//		}
 		Scanner in = new Scanner(System.in);
 		
 		int playerNum = 4;
 		
 		BigTwo game = new BigTwo(playerNum);
-		
-//		for(int i = 0; i < playerNum; i++) {
-//			System.out.println("Player " + (i + 1) +":");
-//			System.out.println(Arrays.toString(game.hand(i)));
-//		}
-////		
-//		ArrayList<Card[]> moves = game.possibleMoves();
-//		System.out.println(moves.size());
-//		
-//		for(int i = 0; i < moves.size(); i++) {
-//			System.out.println(Arrays.toString(moves.get(i)));
-//		}
-//
-//		game.applyMove(0);
-//		
-//		moves = game.possibleMoves();
-//		System.out.println(moves.size());
-//		
-//		for(int i = 0; i < moves.size(); i++) {
-//			System.out.println(Arrays.toString(moves.get(i)));
-//		}
 		
 		while(game.currentTurn() != -1) {
 			System.out.println("It is player " + game.currentTurn() + "'s turn");
